04d5d0e99da94803ee5b5de4b5932ecc1a21f09e
Appsius <ilyas.ustunkaya234434@gmail.com>
2020-02-01 16:20:57 +0300

    LocalStorage w/ useEffect Hooks

--- src/TodoList.js
+++ src/TodoList.js
@@ -5,24 +5,26 @@ import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
 
 export default function TodoList({ todos, removeTodo, toggleTodo, editTodo }) {
-  return (
-    <Paper>
-      <List>
-        {todos.map((todo, i) => (
-          <>
-            <Todo
-              id={todo.id}
-              task={todo.task}
-              key={todo.id}
-              completed={todo.completed}
-              removeTodo={removeTodo}
-              toggleTodo={toggleTodo}
-              editTodo={editTodo}
-            />
-            {i < todos.length - 1 && <Divider />}
-          </>
-        ))}
-      </List>
-    </Paper>
-  );
+  if (todos.length)
+    return (
+      <Paper>
+        <List>
+          {todos.map((todo, i) => (
+            <>
+              <Todo
+                id={todo.id}
+                task={todo.task}
+                key={todo.id}
+                completed={todo.completed}
+                removeTodo={removeTodo}
+                toggleTodo={toggleTodo}
+                editTodo={editTodo}
+              />
+              {i < todos.length - 1 && <Divider />}
+            </>
+          ))}
+        </List>
+      </Paper>
+    );
+  return null;
 }
