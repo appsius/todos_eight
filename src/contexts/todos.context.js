daa71dee5d8391242a60f53ce9368be6f3974f8d
Appsius <ilyas.ustunkaya234434@gmail.com>
2020-02-02 12:55:03 +0300

    Adding Todos Context

---
+++ src/contexts/todos.context.js
@@ -0,0 +1,17 @@
+import React, { createContext } from 'react';
+import useTodoState from '../hooks/useTodoState';
+
+const defaultTodos = [
+  { id: 1, task: 'Move move move move move', completed: false },
+  { id: 2, task: 'ask ask ask ask ask ask', completed: true }
+];
+export const TodosContext = createContext();
+
+export default function TodosProvider(props) {
+  const todosStuff = useTodoState(defaultTodos);
+  return (
+    <TodosContext.Provider value={todosStuff}>
+      {props.children}
+    </TodosContext.Provider>
+  );
+}
