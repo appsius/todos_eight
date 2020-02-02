af56bba0c257ebcdb14bea0e62c6c7d9b5815c2e
Appsius <ilyas.ustunkaya234434@gmail.com>
2020-02-02 16:47:33 +0300

    Splitting into 2 Contexts

--- src/contexts/todos.context.js
+++ src/contexts/todos.context.js
@@ -5,13 +5,17 @@ const defaultTodos = [
   { id: 1, task: 'Move move move move move', completed: false },
   { id: 2, task: 'ask ask ask ask ask ask', completed: true }
 ];
+
 export const TodosContext = createContext();
+export const DispatchContext = createContext();
 
 export function TodosProvider(props) {
   const [todos, dispatch] = useReducer(todoReducer, defaultTodos);
   return (
-    <TodosContext.Provider value={{ todos, dispatch }}>
-      {props.children}
+    <TodosContext.Provider value={todos}>
+      <DispatchContext.Provider value={dispatch}>
+        {props.children}
+      </DispatchContext.Provider>
     </TodosContext.Provider>
   );
 }
